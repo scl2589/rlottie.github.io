27089976a23b61471e6f1fadb771875a0c797a33
tlsdorye <tlsdorye@gmail.com>
2020-10-01 03:27:38 +0900

    Update thumbnail.js

---
+++ thumbnail.js
@@ -0,0 +1,12 @@
+
+class ThumbnailHandler {
+    rlotties = [];
+    rafId = 0;
+    frameCount = 0;
+
+    constructor(topLevelLayers) {
+        for(let i = 0; i < topLevelLayers.length; i++) {
+            
+        }
+    }
+}
